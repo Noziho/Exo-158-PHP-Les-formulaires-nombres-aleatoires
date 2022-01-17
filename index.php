3cec8af738e05e5ebc5bab41c20520c5925e0b57
Noah2211 <noah.decroix3@gmail.com>
2022-01-17 15:58:11 +0100

    done

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
-<?php
-/*
-- Créer un script php qui permet de générer un nombre aléatoire compris entre x et y
-- Coté front, créer un formulaire acceptant deux valeurs : x et y
-- Envoyer le formulaire au script php et retourner le nombre aleatoire généré
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="formTreatment.php" method="post">
+    <label for="x"></label>
+    <input type="text" id="x" name="x">
 
+    <label for="y"></label>
+    <input type="text" id="y" name="y">
 
-- Pour générer un nombre aléatoire en PHP , vous pouvez utiliser la méthode mt_rand() ou rand()
-
-http://php.net/manual/fr/function.mt-rand.php
-*/
+    <input type="submit" name="Envoyez">
+</form>
+</body>
+</html>
